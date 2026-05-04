d562bbfb95b3ae93a1c50fc4f1760aa52e750ba6
Samii Shabuse <sshabuse@gmail.com>
2026-05-04 15:18:43 -0400

    reformat

--- src/functions/tests/main.py
+++ src/functions/tests/main.py
@@ -78,7 +78,7 @@ def lambda_handler(event, context):
             body = json.loads(event.get("body", "{}"))
             required = [
                 "vgcpid",
-              # "request_id",
+                # "request_id",
                 "requires_dat",
                 "requires_oet",
                 "due_date",
